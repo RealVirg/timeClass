a305a80415a49755d4bafceb286a1e37c27f7b83
RealVirg <49589317+RealVirg@users.noreply.github.com>
2024-12-16 12:43:14 +0500

    add file

---
+++ timeClass.py
@@ -0,0 +1,39 @@
+import time
+
+
+class GameTime:
+    def __init__(self) -> None:
+        self.days = 0
+        self.hours = 0
+        self.minutes = 0
+        
+        self.update_speed = 30
+
+        self.current_name_day = 0
+
+        self.day_list = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"]
+    
+    def get_current_name_day(self):
+        return self.day_list[self.current_name_day]
+    
+    def update(self):
+        self.minutes += self.update_speed
+        added_hours = self.minutes // 60
+        self.minutes = self.minutes % 60
+        self.hours += added_hours
+        self.days += self.hours // 24
+        self.hours = self.hours % 24
+        self.current_name_day = self.days % 7
+
+    def show_time(self):
+        return f"{self.get_current_name_day()} {self.hours}:{self.minutes:02d}"
+
+
+gt = GameTime()
+
+gt.get_current_name_day()
+while gt.days < 7:
+    print(f"\r" + " " * 50, end="")
+    print(f"\r{gt.show_time()}", end="")
+    gt.update()
+    time.sleep(1)
